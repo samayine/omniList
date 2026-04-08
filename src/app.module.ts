a813a2d53b7c0de7fac1dd2de3eeabc3185f64fe
samayine <samiayine3@gmail.com>
2026-04-08 20:02:46 +0300

    chore: initial project scaffolding

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PropertiesModule } from './properties/properties.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { AdminModule } from './admin/admin.module';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    PropertiesModule,
+    FavoritesModule,
+    AdminModule,
+    CommonModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
